dfdf8138aa38be4cb047581f5ecda44d26500301
yolo <1084677348@qq.com>
2020-04-22 18:10:06 +0800

    [+]添加eslint配置文件(待完善)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   targets: "activity", //设置是打包modules文件还是activity文件
-  dirName: "51day", //要打包的目录
+  dirName: "mayDay", //要打包的目录
   title: "五一有假，浪漫无价！",
   jweixin: true,
   linkedme: true
