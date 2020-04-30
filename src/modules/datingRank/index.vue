bf2260fe807c82ae69f54982e8c8433c312ba413
yolo <1084677348@qq.com>
2020-04-30 10:14:49 +0800

    [*]真爱榜页面完善

--- src/modules/datingRank/index.vue
+++ src/modules/datingRank/index.vue
@@ -2,19 +2,27 @@
   <div class="page">
     <div class="page-bd">
       <div class="">
-        <ul class="list">
-          <li class="item" v-for="item in list">
-            <p>{{ item.loveValue }}</p>
-          </li>
-        </ul>
+        <van-list :offset="10" :immediate-check="false">
+          <ul class="list">
+            <li class="item" v-for="item in list">
+              <div class="hd"></div>
+              <div class="bd"></div>
+              <div class="ft"></div>
+            </li>
+          </ul>
+        </van-list>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Vue from 'Vue';
+import { List } from 'vant';
+Vue.use(List);
+
 import service from '@/api';
 export default {
-  name: '',
+  name: 'datingRank',
   data() {
     return {
       query: {
@@ -43,4 +51,21 @@ export default {
   },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@function px2rem($px, $rem: 75) {
+  @return $px / $rem + rem;
+}
+body {
+  background-color: #f0f0f0;
+}
+.page-bd {
+  padding: px2rem(30);
+}
+.list {
+  li {
+    min-height: 160px;
+    margin-bottom: 10px;
+    background-color: #fff;
+  }
+}
+</style>
