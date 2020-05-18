1b2440aa37974e9bd6b0cbbaba8ede5298aba792
yolo <1084677348@qq.com>
2020-05-18 18:13:16 +0800

    [*]活动添加是否可以分享功能

--- src/activity/mayDay/js/index.js
+++ src/activity/mayDay/js/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
 import index from '../index';
-//引入rem支持
+// 引入rem支持
 import '@/utils/flexible';
-//引入公共样式
+// 引入公共样式
 import '@/styles/site.scss';
-//测试或正式环境判断
+// 测试或正式环境判断
 import { getQueryString } from '@/utils';
+import { initNav } from '@/utils/appNativeFun';
 
+// 获取url的参数
 let search = getQueryString();
 
 // 开发环境
 if (location.href.indexOf('beta') > -1) {
-  console.log('开发环境');
   try {
     new VConsole();
   } catch (e) {
@@ -21,6 +22,24 @@ if (location.href.indexOf('beta') > -1) {
   if (search.debug) {
     new VConsole();
   }
+}
+
+// 是否可分享
+if (search.initNav && search.initNav != 'undefined') {
+  // 分享信息
+  let shareInfo = {
+    type: 2,
+    data: {
+      msg: 'wewawa',
+      link: location.href,
+      title: '五一有假，浪漫无假', // 分享标题
+      showUrl: location.href,
+      imgUrl: '',
+      desc: '爱情需要诺言更需要陪伴，陪伴是最长情的告白', // 分享描述
+    },
+  };
+  // 生成分享
+  initNav(shareInfo);
 }
 
 //实例化操作
