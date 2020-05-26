e22eca03a516e6ff2ad9fc73f99ba424a1d557c8
yolo <1084677348@qq.com>
2020-05-26 18:10:06 +0800

    :tada: ios更新页交互体验优化

--- src/modules/appUpdate/index.vue
+++ src/modules/appUpdate/index.vue
@@ -3,11 +3,19 @@
     <div class="page-bd">
       <div class="update-box">
         <a class="update" href="" style="display:none"></a>
-        <button type="button" class="btn" @click="handle($event)">
+        <button
+          type="button"
+          class="install"
+          :class="{ download: isClick }"
+          @click="handle($event)"
+        >
           下载安装
         </button>
         <div class="tips">
-          <p></p>
+          <!-- <p
+            class="download"
+            style=""
+          ></p> -->
         </div>
       </div>
     </div>
@@ -19,6 +27,7 @@ export default {
   name: 'appUpdate',
   data() {
     return {
+      isClick: false,
       query: {
         password: '',
         location_href: 'https://www.jikeqi.com/6VRz',
@@ -33,37 +42,55 @@ export default {
         document.querySelector('.update').setAttribute('href', downurl);
       }
     });
-    document.addEventListener('click', (e) => {
-      console.log(e.target);
-      console.log(e);
-      // 只读属性
-      // if (!e.isTrusted) {
-      //   console.log('用户行为点击的');
-      // } else {
-      //   console.log('不是用户行为点击的');
-      // }
-    });
+    // document.addEventListener('click', (e) => {
+    //   console.log(e.target);
+    //   console.log(e);
+    // });
   },
   methods: {
     handle(e) {
       console.log(e);
-      e.target.innerText = '正在安装，请按 Home 键在桌面查看';
       e.target.disabled = true;
-      // 触发a标签的点击事件
+      let instll = document.querySelector('.install');
       let a = document.querySelector('.update');
-      a.click();
+      instll.style.width = '40px';
+      setTimeout(() => {
+        this.isClick = true;
+        // 触发a标签的点击事件
+        a.click();
+      }, 300);
     },
   },
 };
 </script>
 <style lang="scss">
-.btn {
+.update-box {
+  text-align: center;
+}
+.install {
+  width: 200px;
+  height: 40px;
   color: #fff;
-  border: 1px solid rgb(0, 198, 255) !important;
-  background: linear-gradient(
-    to right bottom,
-    rgb(65, 0, 185),
-    rgb(0, 198, 255)
-  );
+  transition: all 0.25s;
+  border: 1px solid #cb1d3d;
+  background-color: #cb1d3d;
+}
+.download {
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  border-top-color: transparent;
+  border-left-color: transparent;
+  animation: rotate 0.6s linear infinite;
+  background-color: transparent;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
