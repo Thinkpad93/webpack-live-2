c6c5670b3b6d806b746712ac1d3959193f5232b3
yolo <1084677348@qq.com>
2020-07-01 13:56:45 +0800

    :whale: 更新dist/modules/rules文件夹

--- dist/modules/rules/alias.js
+++ dist/modules/rules/alias.js
@@ -6,6 +6,8 @@
   var address = document.querySelector('.address'); // 联系电话
   var tel = document.querySelector('.tel'); // 联系电话
   var email = document.querySelector('.email'); // 联系电话
+  var trading = document.querySelectorAll('.trading'); // 贷币体系
+  var origin = location.origin; // 
   var platArray = [
     { 
       id: 1, 
@@ -14,7 +16,9 @@
       abbreviation: '飞雁科技',
       address: '成都市成华区二环路东一段29号3楼7号',
       tel: '020-85686599',
-      email: '877977688@qq.com' 
+      email: '877977688@qq.com',
+      trading: '金豆',
+      origin: '' 
     },
     { 
       id: 2, 
@@ -22,8 +26,10 @@
       company: '深圳而立之年网络科技有限公司', 
       abbreviation: '而立之年',
       address: '深圳市南山区粤海街道高新南七道1号粤美特大厦1302',
-      tel: '18520489959',
-      email: '305837537@qq.com'        
+      tel: '020-85686606',
+      email: '2630937037@qq.com',
+      trading: '金豆',
+      origin: 'www.cdsfl8888.com'        
     },
     {
       id: 3, 
@@ -32,22 +38,40 @@
       abbreviation: '华兀科技',
       address: '深圳市南山区南山街道红花园社区深南大道12069号海岸时代公寓西座2909',
       tel: '18211472440',
-      email: '430106807@qq.com'      
+      email: '430106807@qq.com',
+      trading: '金豆',
+      origin: 'betapack.cdsfl8888.com'      
     }
   ];
+  
   function getObjInfo(obj) {
     var id = null;
-    if (obj.bibi) {
-      id = 1;
-    } else if (obj.duoduo) {
-      id = 2;
+    // app内
+    if (obj.app) {
+      console.log('app内');
+      if (obj.bibi) {
+        id = 1;
+      } else if (obj.duoduo) {
+        id = 2;
+      } else {
+        id = 3;
+      }
     } else {
-      id = 3;
+      console.log('app外');
+      platArray.forEach(function(item) {
+        var _origin = item.origin;
+        if (_origin.length) {
+          if (origin.indexOf(_origin) > -1) {
+            id = item.id;
+          }
+        }
+      });
     }
+    // 返回对象
     var f = platArray.filter(function (item) {
       return item.id === id;
     })[0];
-    return f;
+    return f;    
   }
   var result = getObjInfo(brow);
   console.log(result);
@@ -76,6 +100,12 @@
       address.innerHTML = result.address;
       tel.innerHTML = result.tel;
       email.innerHTML = result.email;
+    }
+    // 贷币体系
+    if (trading.length) {
+      for (var i = 0; i < trading.length; i++) {
+        trading[i].innerHTML = result.trading;
+      }      
     } 
   }
 })(window, document);
